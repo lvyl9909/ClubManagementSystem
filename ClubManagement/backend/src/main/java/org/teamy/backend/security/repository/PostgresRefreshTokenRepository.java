063a1223736d5e2b22e513c53f95319f5be85299
Crystallen1 <yunchen6@student.unimelb.edu.au>
2024-09-05 14:10:27 +1000

    fix

--- ClubManagement/backend/src/main/java/org/teamy/backend/security/repository/PostgresRefreshTokenRepository.java
+++ ClubManagement/backend/src/main/java/org/teamy/backend/security/repository/PostgresRefreshTokenRepository.java
@@ -81,7 +81,8 @@ public class PostgresRefreshTokenRepository implements RefreshTokenRepository{
 
         String sql = "DELETE FROM refresh_token WHERE id = ?";
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setString(1, id);
+            statement.setObject(1,UUID.fromString(id));
+//            statement.setString(1, id);
             statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
