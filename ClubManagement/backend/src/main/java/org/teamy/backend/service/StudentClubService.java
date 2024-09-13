73fa1fddecde0221135ee93e1d607ab94bfa23d0
Crystallen1 <yunchen6@student.unimelb.edu.au>
2024-09-13 22:26:56 +1000

    fix

--- ClubManagement/backend/src/main/java/org/teamy/backend/service/StudentClubService.java
+++ ClubManagement/backend/src/main/java/org/teamy/backend/service/StudentClubService.java
@@ -1,28 +1,33 @@
 package org.teamy.backend.service;
 
-import org.teamy.backend.DataMapper.StudentClubDataMapper;
-import org.teamy.backend.model.Student;
-import org.teamy.backend.repository.RSVPRepository;
+import org.teamy.backend.repository.ClubRepository;
 import org.teamy.backend.repository.StudentClubRepository;
+import org.teamy.backend.repository.StudentRepository;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public class StudentClubService {
     private final StudentClubRepository studentClubRepository;
+    private final StudentRepository studentRepository;
+    private final ClubRepository clubRepository;
     private static StudentClubService instance;
-    public static synchronized StudentClubService getInstance(StudentClubRepository studentClubRepository) {
+    public static synchronized StudentClubService getInstance(StudentClubRepository studentClubRepository,StudentRepository studentRepository, ClubRepository clubRepository) {
         if (instance == null) {
-            instance = new StudentClubService(studentClubRepository);
+            instance = new StudentClubService(studentClubRepository, studentRepository,clubRepository );
         }
         return instance;
     }
-    private StudentClubService(StudentClubRepository studentClubRepository) {
+    private StudentClubService(StudentClubRepository studentClubRepository, StudentRepository studentRepository, ClubRepository clubRepository) {
         this.studentClubRepository = studentClubRepository;
+        this.studentRepository = studentRepository;
+        this.clubRepository = clubRepository;
     }
     public void addAdmin(Integer clubId,Integer studentId){
         try {
             studentClubRepository.addNewAdmin(clubId,studentId);
+            studentRepository.invalidateStudentCache(studentId);
+            clubRepository.invalidateClubCache(clubId);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -30,6 +35,8 @@ public class StudentClubService {
     public void deleteAdmin(Integer clubId,Integer studentId){
         try {
             studentClubRepository.deleteAdmin(clubId,studentId);
+            studentRepository.invalidateStudentCache(studentId);
+            clubRepository.invalidateClubCache(clubId);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
